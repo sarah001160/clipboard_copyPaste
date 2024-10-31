0592240067eb4a4b6fda9d9bfbb9fdbab11e0172
sarah001160 <sarah001160@gmail.com>
2024-11-01 01:00:20 +0800

    複製文字時也複製格式，貼在word檔會維持文字格式

--- src/js/all.js
+++ src/js/all.js
@@ -7,19 +7,30 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     },
     methods: {
-      copyTxt(text) {
-        if (navigator.clipboard) {
-          // 使用 Clipboard API
-          return navigator.clipboard.writeText(text).then(() => {
-            alert('文本已複製到剪貼簿');
-          }).catch(err => {
-            alert('無法複製文本：', err)
-            console.error('無法複製文本：', err);
-          });
+      copyTxt(html) {
+        try {
+          navigator.clipboard.write([
+            new ClipboardItem({
+              'text/html': new Blob([html], { type: 'text/html' })
+            })
+          ]);
+          alert('文本已複製到剪貼簿');
+        } catch (err) {
+          alert('無法複製文本：' + err.message);
+          console.error('無法複製文本：', err);
         }
+        // if (navigator.clipboard) {
+        //   // 使用 Clipboard API
+        //   return navigator.clipboard.writeText(text).then(() => {
+        //     alert('文本已複製到剪貼簿');
+        //   }).catch(err => {
+        //     alert('無法複製文本：', err)
+        //     console.error('無法複製文本：', err);
+        //   });
+        // }
       },
       getRef(tag) {
-        const text = this.$refs[tag].textContent; // 原來有這種寫法 $refs[tag] 等同 $refs.text1 (tag代表近來的參數值text1)
+        const text = this.$refs[tag].innerHTML; // 原來有這種寫法 $refs[tag] 等同 $refs.text1 (tag代表近來的參數值text1)
         this.copyTxt(text); // 剪貼簿
       }
     },
