affd488623f1fb73838213e6afb5798e0e178bd5
sarah001160 <sarah001160@gmail.com>
2024-11-03 23:48:24 +0800

    增加readme、可複製純文字、html格式
    - 可貼在記事本，但html 標籤(純文字)也會秀出來
    - 可貼在word檔，html標籤不會出現

---
+++ readme.md
@@ -0,0 +1,8 @@
+# 點擊按鈕，複製文字
+
+## 使用
+
+- navigator.clipboard.write()
+- [new ClipboardItem({...})]
+- {'text/html': new Blob([html], { type: 'text/html' })}
+- {'text/plain': new Blob([html], { type: 'text/plain' })}
--- src/js/all.js
+++ src/js/all.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     data() {
       return {
         text: 'hello 這是要複製的文本內容',
-
       }
     },
     methods: {
@@ -11,7 +10,8 @@ document.addEventListener('DOMContentLoaded', () => {
         try {
           navigator.clipboard.write([
             new ClipboardItem({
-              'text/html': new Blob([html], { type: 'text/html' })
+              'text/html': new Blob([html], { type: 'text/html' }), //你正在處理的數據是 HTML 格式的內容，可以包含標籤、結構和樣式
+              'text/plain': new Blob([html], { type: 'text/plain' }) // 你要寫入剪貼簿的資料類型是純文本
             })
           ]);
           alert('文本已複製到剪貼簿');
@@ -19,15 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
           alert('無法複製文本：' + err.message);
           console.error('無法複製文本：', err);
         }
-        // if (navigator.clipboard) {
-        //   // 使用 Clipboard API
-        //   return navigator.clipboard.writeText(text).then(() => {
-        //     alert('文本已複製到剪貼簿');
-        //   }).catch(err => {
-        //     alert('無法複製文本：', err)
-        //     console.error('無法複製文本：', err);
-        //   });
-        // }
       },
       getRef(tag) {
         const text = this.$refs[tag].innerHTML; // 原來有這種寫法 $refs[tag] 等同 $refs.text1 (tag代表近來的參數值text1)
